f412aefd1ae4e584f8d2a2d94447eb8eb497fcac
Zichen Zhang <zhangzzc@umich.edu>
2025-06-22 05:19:11 -0700

    Add Chatbox and NewsletterForm to Home component layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -34,7 +34,9 @@ export default function Home() {
     <div className="min-h-screen bg-white">
       <Header />
       <main className="container mx-auto px-4 py-8">
+        <Chatbox />
         {articles.length > 0 && <ArticleGrid articles={articles} />}
+        <NewsletterForm />
       </main>
       <Footer />
     </div>
